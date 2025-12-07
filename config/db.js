a48eeb128e21f9d0263ac1ba1d4d2371f7b240e0
shaikhjunaidi <junnuedits@gmail.com>
2025-12-07 12:02:12 +0530

    Fix database connection with SSL

--- config/db.js
+++ config/db.js
@@ -5,12 +5,11 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT || 3306, // Good practice to add port too
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 module.exports = pool.promise();
